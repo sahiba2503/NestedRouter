274f74fdf646751a84e8ac0b17306347d11acbc5
sahiba <25sahiba03@gmail.com>
2026-07-02 22:57:16 +0530

    add useContext to use value in grandChild component

--- src/Component/HomeListItemA.jsx
+++ src/Component/HomeListItemA.jsx
@@ -1,33 +1,3 @@
-// // import React from 'react'
-// // import { createContext,useContext } from 'react'
-// // import {userContext} from "./App.jsx";
-// // function HomeListItemA() {
-// //    const user = useContext(userContext)
-// //   return (
-// //     <div>
-// //       <h2>Title - {user} </h2>
-// //       home list item aaaa
-// //     </div>
-// //   )
-// // }
-
-// // export default HomeListItemA
-
-// import React, { useContext } from "react";
-// import { userContext } from "./App";
-
-// function HomeListItemA() {
-//   const user = useContext(userContext);
-
-//   return (
-//     <div>
-//       <h2>Title - {user}</h2>
-//       home list item aaaa
-//     </div>
-//   );
-// }
-
-// export default HomeListItemA;
 
 import React, { useContext } from "react";
 import { userContext } from "../App";
