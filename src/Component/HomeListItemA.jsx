59714cb9556ac5cad4bc7bd8b3fba8787397466c
sahiba <25sahiba03@gmail.com>
2026-07-03 21:50:02 +0530

    add update method

--- src/Component/HomeListItemA.jsx
+++ src/Component/HomeListItemA.jsx
@@ -1,15 +1,54 @@
-import React, { useContext } from "react";
+
+import React, { useState, useContext } from "react";
 import { userContext } from "../App";
 
 function HomeListItemA() {
   const user = useContext(userContext);
 
+  const [name, setName] = useState([
+    { id: 1, name: "sahiba" },
+    { id: 2, name: "sana" },
+    { id: 3, name: "samama" },
+  ]);
+  function updateName(key) {
+    const updatedNameList = name.map((val) => {
+      if (val.id === key) {
+        return {
+          id:val.id,       
+          name: "madam",
+        };
+      } 
+else {
+     return val;
+      }
+    });
+    setName(updatedNameList);
+  }
+
   return (
     <div>
-      <h2>Title - {user}</h2>
-      <p>Home List Item A</p>
+      <h2>User Name</h2>
+
+      {name.map((value) => (
+        <div
+          key={value.id}
+          onClick={() => updateName(value.id)}
+        >
+          {value.name}
+        </div>
+      ))}
+      <p>Update Name</p>
+
+      <button>Update User Name</button>
     </div>
   );
 }
 
 export default HomeListItemA;
+
+
+
+
+
+
+
