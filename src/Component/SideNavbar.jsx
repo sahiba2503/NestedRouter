d7f9fdb10f6221d49f945ba007b51522e13ab9c0
sahiba <25sahiba03@gmail.com>
2026-07-07 23:51:47 +0530

    add side nave list items

--- src/Component/SideNavbar.jsx
+++ src/Component/SideNavbar.jsx
@@ -1,62 +1,97 @@
-// import React from "react";
-// import { useNavigate } from "react-router-dom";
+// // import React from "react";
+// // import { useNavigate } from "react-router-dom";
+
+// // function SideNavbar() {
+// //   let navigate = useNavigate();
+// //   return (
+// //     <div className='sideNavList'>
+// //       <ul>
+// //         <li
+// //           onClick={() => {
+// //             navigate("/heroSection");
+// //           }}
+// //         >
+// //           heroSection
+// //         </li>
+// //         <li
+// //           onClick={() => {
+// //             navigate("/welcome");
+// //           }}
+// //         >
+// //           welcome
+// //         </li>
+// //         <li
+// //           onClick={() => {
+// //             navigate("/featuredServices");
+// //           }}
+// //         >
+// //           featuredServices
+// //         </li>
+// //       </ul>
+// //     </div>
+// //   );
+// // }
+
+// // export default SideNavbar;
+// import React from 'react'
+// import {useLocation} from "react-router-dom";
 
 // function SideNavbar() {
-//   let navigate = useNavigate();
+//   const pathname = useLocation();
+//   let list = [];
+//   if(pathname === "/"){
+//     list = ["heroSection" , "welcome" , "featuredServices"];
+//   }
+//   else if(pathname === "/Contact"){
+//     list = ["Emial", "Pone", "Support"];
+//   }
+//   else if(pathname === "/About"){
+//     list = ["Profile", "Settings","History"];
+//   }
 //   return (
-//     <div className='sideNavList'>
+//     <div>
 //       <ul>
-//         <li
-//           onClick={() => {
-//             navigate("/heroSection");
-//           }}
-//         >
-//           heroSection
-//         </li>
-//         <li
-//           onClick={() => {
-//             navigate("/welcome");
-//           }}
-//         >
-//           welcome
-//         </li>
-//         <li
-//           onClick={() => {
-//             navigate("/featuredServices");
-//           }}
-//         >
-//           featuredServices
-//         </li>
+// {list.map((item,index)=>(
+//   <li key={index}>{item}</li>
+// ))}
 //       </ul>
 //     </div>
-//   );
+//   )
 // }
 
-// export default SideNavbar;
-import React from 'react'
-import {useLocation} from "react-router-dom";
+// export default SideNavbar
+
+import React from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 function SideNavbar() {
-  const pathname = useLocation();
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+
   let list = [];
-  if(pathname === "/"){
-    list = ["Company","Mission","Team"];
+
+  if (pathname === "/") {
+    list = ["heroSection", "welcome", "featuredServices"];
+  } else if (pathname === "/contact") {
+    list = ["Email", "Phone", "Support"];
+  } else if (pathname === "/about") {
+    list = ["Profile", "Settings", "History"];
   }
-  else if(pathname === "/Contact"){
-    list = ["Emial", "hPone", "Support"];
-  }
-  else if(pathname === "/About"){
-    list = ["Profile", "Settings","History"];
-  }
+
   return (
     <div>
       <ul>
-{list.map((item,index)=>(
-  <li key={index}>{item}</li>
-))}
+        {list.map((item, index) => (
+          <li
+            key={index}
+            onClick={() => navigate(`/${item}`)}            
+          >
+            {item}
+          </li>
+        ))}
       </ul>
     </div>
-  )
+  );
 }
 
-export default SideNavbar
+export default SideNavbar;
