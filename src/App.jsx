306cf06262c97f1daa1295f8f028d5029dc2877d
sahiba <25sahiba03@gmail.com>
2026-07-10 21:30:52 +0530

    add useEffect hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React, { createContext } from "react";
 import Navlist from "./Component/Navlist";
 import RouterContainer from "./RouterContainer";
-
+import {useEffect} from "react"
 export const userContext = createContext();
 
 function App() {
   const title = "hello sahiba";
+  
+  useEffect(() => {
+  alert("Do you really want to open?");
+}, []);
   
 
   return (
